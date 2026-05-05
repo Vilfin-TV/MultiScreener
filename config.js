f6868fb908c79aacbbec5b08f877d516aae115d0
Vilfin-TV <filmonjoseph@gmail.com>
2026-05-05 11:48:02 +0900

    Hide worker URL and switch Manorama feed

--- config.js
+++ config.js
@@ -11,9 +11,6 @@ const VILTV_CONFIG = {
   // Default AI model
   DEFAULT_MODEL: 'gemini-2.5-flash',
 
-  // First-party Worker used for AI, scores, and feed proxying
-  WORKER_URL: 'https://x9-k2-p30-worker.workers.dev',
-
   // App version
   VERSION: 'v9.0',
 };
