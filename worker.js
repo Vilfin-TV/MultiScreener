e7ea5c44068313c72d682e92ed11842b7137aada
Vilfin-TV <filmonjoseph@gmail.com>
2026-05-11 21:32:23 +0900

    Fix: add vilfintv.com to CORS allowed origins in Cloudflare Worker

--- worker.js
+++ worker.js
@@ -41,6 +41,7 @@
 const ALLOWED_ORIGINS = [
   'https://vilfin-tv.github.io',
   'https://vilfintv.github.io',
+  'https://vilfintv.com',
   'http://127.0.0.1:5500',
   'http://localhost:5500',
   'http://localhost:3000',
